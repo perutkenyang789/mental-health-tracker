a902a0dfb526d945e0d6fbe9559de657f1c48707
unknown <MAHASISWA@CSLAB-A102-19.ms.ui.ac.id>
2024-09-18 13:29:48 +0700

    Create logout function

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from main.forms import MoodEntryForm
@@ -70,3 +70,7 @@ def login_user(request):
       form = AuthenticationForm(request)
    context = {'form': form}
    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('main:login')
